aac1838db43a1eb26b632b08058b1b24c0492b74
owl <143802335+user153AZER@users.noreply.github.com>
2023-09-09 23:14:26 -0400

    Create Milky Way Visualization.py

---
+++ Milky Way Visualization.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Generate random star positions
+num_stars = 1000
+x = np.random.randn(num_stars)
+y = np.random.randn(num_stars)
+z = np.random.randn(num_stars)
+
+# Plot the stars
+fig = plt.figure(figsize=(8, 8))
+ax = fig.add_subplot(111, projection='3d')
+ax.scatter(x, y, z, c='white', marker='.', s=1)
+
+# Set plot limits and labels
+ax.set_xlim(-10, 10)
+ax.set_ylim(-10, 10)
+ax.set_zlim(-10, 10)
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('Z')
+
+# Set plot title
+plt.title('Milky Way Visualization')
+
+# Show the plot
+plt.show()
